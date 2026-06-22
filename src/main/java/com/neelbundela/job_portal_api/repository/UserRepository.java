5b26d9f4232be3ba3d4f13fc725d486f0f8dea8f
NEELBundela <neelbundela1807@gmail.com>
2026-06-22 17:25:10 +0530

    Created UserRepository interface and write one method.

---
+++ src/main/java/com/neelbundela/job_portal_api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.neelbundela.job_portal_api.repository;
+
+import com.neelbundela.job_portal_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+}
