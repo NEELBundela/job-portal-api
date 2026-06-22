a90c1287b2195b7a2969216106941364b7aabdeb
NEELBundela <neelbundela1807@gmail.com>
2026-06-22 18:14:03 +0530

    Add SEcurity FilterChain.

--- src/main/java/com/neelbundela/job_portal_api/config/SecurityBeans.java
+++ src/main/java/com/neelbundela/job_portal_api/config/SecurityBeans.java
@@ -2,8 +2,11 @@ package com.neelbundela.job_portal_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityBeans {
@@ -11,5 +14,19 @@ public class SecurityBeans {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http)
+        throws Exception{
+
+        http
+                .csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 }
