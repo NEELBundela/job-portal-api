b1cb55b0ce9f357f2977ef610a605040d7806136
NEELBundela <neelbundela1807@gmail.com>
2026-06-22 17:08:46 +0530

    Created User Entity

---
+++ src/main/java/com/neelbundela/job_portal_api/entity/User.java
@@ -0,0 +1,31 @@
+package com.neelbundela.job_portal_api.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
