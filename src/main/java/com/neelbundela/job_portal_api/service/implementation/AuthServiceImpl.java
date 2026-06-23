76f27fdff742301b76e92fcbdee293e4da7cb7f5
NEELBundela <neelbundela1807@gmail.com>
2026-06-23 16:53:55 +0530

    Update AuthService Implementation.

--- src/main/java/com/neelbundela/job_portal_api/service/implementation/AuthServiceImpl.java
+++ src/main/java/com/neelbundela/job_portal_api/service/implementation/AuthServiceImpl.java
@@ -1,8 +1,11 @@
 package com.neelbundela.job_portal_api.service.implementation;
 
+import com.neelbundela.job_portal_api.dto.LoginRequest;
+import com.neelbundela.job_portal_api.dto.LoginResponse;
 import com.neelbundela.job_portal_api.dto.RegistrationRequest;
 import com.neelbundela.job_portal_api.entity.User;
 import com.neelbundela.job_portal_api.repository.UserRepository;
+import com.neelbundela.job_portal_api.security.JwtService;
 import com.neelbundela.job_portal_api.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +17,7 @@ public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     @Override
     public String register(RegistrationRequest registrationRequest) {
@@ -35,5 +39,19 @@ public class AuthServiceImpl implements AuthService {
 
         userRepository.save(user);
         return "User Registered Successfully";
+    }
+
+    @Override
+    public LoginResponse login(LoginRequest loginRequest){
+        User user = userRepository.findByEmail(loginRequest.getEmail())
+                .orElseThrow(()-> new RuntimeException("User not found"));
+
+        if(!passwordEncoder.matches(loginRequest.getPassword(),user.getPassword())){
+            throw new RuntimeException("Invalid Credentials");
+        }
+
+        String token=jwtService.generateToken(user);
+
+        return new LoginResponse(token,"Login successful");
     }
 }
