f0e522376f97485d31e4cd8f332ccf9850492105
NEELBundela <neelbundela1807@gmail.com>
2026-06-23 16:52:52 +0530

    Update AuthService interface and create login method.

--- src/main/java/com/neelbundela/job_portal_api/service/AuthService.java
+++ src/main/java/com/neelbundela/job_portal_api/service/AuthService.java
@@ -1,7 +1,10 @@
 package com.neelbundela.job_portal_api.service;
 
+import com.neelbundela.job_portal_api.dto.LoginRequest;
+import com.neelbundela.job_portal_api.dto.LoginResponse;
 import com.neelbundela.job_portal_api.dto.RegistrationRequest;
 
 public interface AuthService {
     String register(RegistrationRequest registrationRequest);
+    LoginResponse login(LoginRequest loginRequest);
 }
