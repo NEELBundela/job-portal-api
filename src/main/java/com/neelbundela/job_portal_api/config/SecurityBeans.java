18cfc0d6e92dd2197afdd5c5d92a077c922dcde7
NEELBundela <neelbundela1807@gmail.com>
2026-06-25 16:27:08 +0530

    Update SecurityBeans class.

--- src/main/java/com/neelbundela/job_portal_api/config/SecurityBeans.java
+++ src/main/java/com/neelbundela/job_portal_api/config/SecurityBeans.java
@@ -1,15 +1,22 @@
 package com.neelbundela.job_portal_api.config;
 
+import com.neelbundela.job_portal_api.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
+
 public class SecurityBeans {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -22,10 +29,21 @@ public class SecurityBeans {
 
         http
                 .csrf(csrf->csrf.disable())
+                .sessionManagement(session->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        ))
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/Auth/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/Auth/recruiter/**").hasRole("RECRUITER")
+                        .requestMatchers("/Auth/CANDIDATE/**").hasRole("CANDIDATE")
                         .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
+
 
         return http.build();
     }
