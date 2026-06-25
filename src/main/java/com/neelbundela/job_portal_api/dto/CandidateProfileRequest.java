311d728884170808ac1b80ed8bffe3a0e1c437f6
NEELBundela <neelbundela1807@gmail.com>
2026-06-25 16:43:22 +0530

    Created Candidate Profile Request Dto.

---
+++ src/main/java/com/neelbundela/job_portal_api/dto/CandidateProfileRequest.java
@@ -0,0 +1,14 @@
+package com.neelbundela.job_portal_api.dto;
+
+import lombok.Data;
+
+@Data
+public class CandidateProfileRequest {
+    private String phone;
+
+    private String skills;
+
+    private String experience;
+
+    private String education;
+}
