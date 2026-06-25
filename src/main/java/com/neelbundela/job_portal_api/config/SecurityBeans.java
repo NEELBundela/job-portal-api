e0796e9435d38806191fcc12cc29a25e7903239f
NEELBundela <neelbundela1807@gmail.com>
2026-06-25 17:18:55 +0530

    Create Authentication manager.

--- src/main/java/com/neelbundela/job_portal_api/config/SecurityBeans.java
+++ src/main/java/com/neelbundela/job_portal_api/config/SecurityBeans.java
@@ -4,7 +4,9 @@ import com.neelbundela.job_portal_api.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,6 +23,12 @@ public class SecurityBeans {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+        throws Exception{
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
