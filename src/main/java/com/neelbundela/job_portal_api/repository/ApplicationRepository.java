20b3fddcacd3f85d9e545e2b2db001a7d9c44b26
NEELBundela <neelbundela1807@gmail.com>
2026-06-29 16:42:40 +0530

    Create a ApplicationRepository Interface.

---
+++ src/main/java/com/neelbundela/job_portal_api/repository/ApplicationRepository.java
@@ -0,0 +1,18 @@
+package com.neelbundela.job_portal_api.repository;
+
+import com.neelbundela.job_portal_api.entity.Application;
+import com.neelbundela.job_portal_api.entity.Job;
+import com.neelbundela.job_portal_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ApplicationRepository extends JpaRepository<Application,Long> {
+
+    List<Application> findByCandidate(User candidate);
+
+    List<Application> findByJobs(Job job);
+
+    Optional<Application> findByCandidateAndJob(User candidate,Job job);
+}
