a9c118c73657a64cc8a5a5c1e7d36c1791870671
NEELBundela <neelbundela1807@gmail.com>
2026-06-30 14:23:04 +0530

    Create a DashboardResponse for Admin.

---
+++ src/main/java/com/neelbundela/job_portal_api/dto/DashboardResponse.java
@@ -0,0 +1,19 @@
+package com.neelbundela.job_portal_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DashboardResponse {
+
+    private long totalUsers;
+    private long totalRecruiters;
+    private long totalCandidates;
+    private long totalJobs;
+    private long totalApplications;
+}
