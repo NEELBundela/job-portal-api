66330aea2e368a89717996521c02ea70041d0049
NEELBundela <neelbundela1807@gmail.com>
2026-06-30 13:58:46 +0530

    Create a ApiResponse Dto file.

---
+++ src/main/java/com/neelbundela/job_portal_api/dto/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.neelbundela.job_portal_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T>{
+
+    private boolean success;
+    private String message;
+    private T data;
+}
